9317b8d7764aac8976c390a1c80929154466f516
Sanyam Jain <sanyam.j@media.net>
2022-07-07 20:15:22 +0530

    First commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
